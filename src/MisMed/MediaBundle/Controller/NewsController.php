31b3f038d97f3287472d2d73d58005c28d654f55
Kangaro97 <kangaro97@gmail.com>
2015-12-12 02:03:26 +0300

    Добавлены верроятные контроллеры для Users и News. Не уверен в них.

---
+++ src/MisMed/MediaBundle/Controller/NewsController.php
@@ -0,0 +1,23 @@
+<?php
+// src/MisMed/MediaBundle/Controller/NewsController.php
+use MisMed\MediaBundle\Entity\News;
+use Symfony\Component\HttpFoundation\Response;
+
+public function createAction($title, $desc_short, $desc_long, $date, $author, $link, $status)
+{
+    $news = new News();
+    $news->setTitle($title);
+    $news->setDescShort($desc_short);
+    $news->setDescLong($desc_long);
+    $news->setDate($date);
+    $news->setAuthor($author);
+    $news->setLink($link);
+    $news->setStatus($status);
+
+    $em = $this->getDoctrine()->getEntityManager();
+    $em->persist($news);
+    $em->flush();
+
+    return new Response('Created product id '.$news->getId());
+}
+?>
---
+++ src/MisMed/MediaBundle/Controller/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+// src/MisMed/MediaBundle/Controller/UsersController.php
+use MisMed\MediaBundle\Entity\Users;
+use Symfony\Component\HttpFoundation\Response;
+
+public function createAction($name, $login, $password, $email, $institute, $sex, $status)
+{
+    $user = new Users();
+    $user->setName($name);
+    $user->setLogin($login);
+    $user->setPassword($password);
+    $user->setEmail($email);
+    $user->setInstitut($institute);
+    $user->setSex($sex);
+    $user->setStatus($status);
+
+    $em = $this->getDoctrine()->getEntityManager();
+    $em->persist($user);
+    $em->flush();
+
+    return new Response('Created product id '.$user->getId());
+}
+?>
